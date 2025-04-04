bbfef0fb437a04b4c858c443d2c9c69ca26df23e
JennyZhang2022 <jenny.zhang3535@outlook.com>
2025-04-04 18:37:17 -0400

    Add StringClientApp project into the solution

---
+++ StringClientApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using StringLib;
+
+namespace StringClientApp
+{
+    internal class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string input = Console.ReadLine();
+
+            if (input.StartsWithUpperCase())
+            {
+                Console.WriteLine("The string starts with an uppercase.");
+            }
+            else
+            {
+                Console.WriteLine("The string starts with an lowercase.");
+            }
+        }
+    }
+}
