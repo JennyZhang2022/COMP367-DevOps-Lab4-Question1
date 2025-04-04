53481be72d147843b82687993a3df6c874a48c34
JennyZhang2022 <jenny.zhang3535@outlook.com>
2025-04-04 18:01:46 -0400

    First Commit for the project.

---
+++ StringLib/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringLib
+{
+    public static class StringExts
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            return !string.IsNullOrEmpty(str) && char.IsUpper(str[0]);
+        }
+    }
+}
